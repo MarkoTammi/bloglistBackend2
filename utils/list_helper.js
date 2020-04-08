4cd8b5aad437185f11955cba1a9c71b7a56451c0
Tammivuori <marko.tammivuori@gmail.com>
2020-04-08 14:37:46 +0300

    4.4: apufunktioita ja yksikkötestejä, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,6 +5,19 @@ const dummy = (blogs) => {
     return 1
 }
 
+
+// TEST - 4.4: apufunktioita ja yksikkötestejä, step2
+// Laskee blogi kirjoitusten "likes" lukumäärän yhteen
+const countTotalLikes = (blogs) => {
+    //console.log('BLOGS', blogs[1].likes)
+    let sum = 0
+    for (const like in blogs) {
+        sum = sum + blogs[like].likes
+    }
+    return sum
+}
+
 module.exports = {
-    dummy
+    dummy,
+    countTotalLikes
 }
