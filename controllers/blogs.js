bb95f07b65aadace7e5391766dd7282c3aaba10c
Tammivuori <marko.tammivuori@gmail.com>
2020-04-21 10:09:23 +0300

    4.19: blogilistan laajennus, step8

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -10,6 +10,17 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const jwt = require('jsonwebtoken')
+
+// Function to fetch token from http request header authorization
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
+
 
 // Get all blogs
 blogsRouter.get('/', async (request, response) => {
@@ -24,12 +35,20 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
+    const token = getTokenFrom(request)
+
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+    //const user = await User.findOne({ username: 'marko@test.fi' })
+
     // Blog's title and URL are manadotory parameters
     if (body.title === undefined || body.url === undefined) {
         return response.status(400).end()
     }
-
-    const user = await User.findOne({ username: 'marko@test.fi' })
 
     const blog = new Blog(
         {
